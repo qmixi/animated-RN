13146a949d8c27da5328aa644a97881d4fc2d5eb
Piotr Kumorek <piotr.kumi@gmail.com>
2018-05-13 17:28:36 +0200

    rotating card out of the screen

--- src/Deck.js
+++ src/Deck.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { View, Animated, PanResponder, Dimensions } from 'react-native';
 
 const SCREEM_WIDTH = Dimensions.get('window').width;
+const SWIPE_THRESHOLD = 0.25 * SCREEM_WIDTH;
+const SWIPE_OUT_DURATION = 250
 
 class Deck extends Component {
     constructor(props) {
@@ -12,17 +14,39 @@ class Deck extends Component {
             onPanResponderMove: (event, gesture) => {
                 position.setValue({ x: gesture.dx, y: gesture.dy });
             },
-            onPanResponderRelease: () => { }
+            onPanResponderRelease: (event, gesture) => {
+                if (gesture.dx > SWIPE_THRESHOLD) {
+                    this.forceSwipe('right');
+                } else if (gesture.dx < -SWIPE_THRESHOLD) {
+                    this.forceSwipe('left');
+                } else {
+                    this.resetPosition();
+                }
+            }
         });
 
         this.state = { panResponder, position };
+    }
+
+    forceSwipe(direction) {
+        const x = direction === 'left' ? -SCREEM_WIDTH : SCREEM_WIDTH;
+        Animated.timing(this.state.position, {
+            toValue: { x: x, y: 0 },
+            duration: SWIPE_OUT_DURATION
+        }).start();
+    }
+
+    resetPosition() {
+        Animated.spring(this.state.position, {
+            toValue: { x: 0, y: 0 }
+        }).start();
     }
 
     getCardStyle() {
         const { position } = this.state;
         const rotate = position.x.interpolate({
             inputRange: [-SCREEM_WIDTH * 1.5, 0, SCREEM_WIDTH * 1.5],
-            outputRange: ['-100deg', '0deg', '100deg']
+            outputRange: ['-125deg', '0deg', '125deg']
         });
 
         return {
