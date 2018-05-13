2713f17da262410c9b03ba2786450f1ec34971de
Piotr Kumorek <piotr.kumi@gmail.com>
2018-05-13 16:50:35 +0200

    prepare animated card

--- App.js
+++ App.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Deck from './src/Deck'
+import { DATA } from './src/common';
+import { Card, Button } from 'react-native-elements';
 
 export default class App extends React.Component {
+
+  renderCard(item) {
+    return (
+      <Card
+        key={item.id}
+        title={item.text}
+        image={{ uri: item.uri }}
+      >
+        <Text>Some text placed on swapping card.</Text>
+        <Button
+          icon={{ name: 'search' }}
+          backgroundColor="#03A9F4"
+          title="View it now!">
+        </Button>
+      </Card>
+
+    )
+  }
+
   render() {
+    console.log('DATA', DATA);
     return (
       <View style={styles.container}>
-        <Deck/>
+        <Deck
+          data={DATA}
+          renderCard={this.renderCard} />
       </View>
     );
   }
--- src/common.js
+++ src/common.js
@@ -1,4 +1,4 @@
-const DATA = [
+export const DATA = [
     { id: 1, text: 'Card #1', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg' },
     { id: 2, text: 'Card #2', uri: 'http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg' },
     { id: 3, text: 'Card #3', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg' },
